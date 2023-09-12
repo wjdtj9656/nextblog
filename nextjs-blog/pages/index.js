e10846b534880f3e61cd787f284ab438b7218558
wjdtj9656 <wjdtj9656@naver.com>
2023-09-13 06:57:59 +0900

    a tag vs link tag

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
+        <h1 className={styles.title}>
+          READ THIS POST{" "}
+          <Link href="/posts/first-post">
+            <a>첫번째 글</a>
+          </Link>
+        </h1>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -55,7 +63,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
