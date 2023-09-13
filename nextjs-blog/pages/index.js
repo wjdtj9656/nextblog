34f03e548242db22fd779a5a419f5f57d6428b1e
wjdtj9656 <wjdtj9656@naver.com>
2023-09-14 07:07:14 +0900

    img vs image

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       </Head>
 
       <main>
+        {/* <img src="/images/image1.png" alt="해골" /> */}
+        <Image src="/images/image1.png" width={140} height={140} />
         <h1 className={styles.title}>
           READ THIS POST{" "}
           <Link href="/posts/first-post">
